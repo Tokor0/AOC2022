50a1ae019b181ea832cf47755737be144441afa7
Tokor0 <git@tokoro.fi>
2022-12-02 04:44:49 +0200

    D1P2 imrovements

--- 1/pt2/calorieCounting2.ml
+++ 1/pt2/calorieCounting2.ml
@@ -13,15 +13,14 @@ let rec sum xs =
   | [] -> 0
   | h :: t -> h + sum t
 
-let pop xs =
-  match xs with
-  | [] -> []
-  | _ :: t -> t
-
 let rec count xs s =
   match maybe_read_int_opt () with
   | None -> sum xs
-  | Some None -> count (pop (ord_insert s xs)) 0
+  | Some None -> begin
+    match xs with
+    | [] -> count (ord_insert s xs) 0
+    | h :: t when (h < s) -> count (ord_insert s t) 0
+    | _ :: _ -> count xs 0 end
   | Some Some n -> count xs (s + n)
 
 let () = print_int (count [0; 0; 0] 0)
